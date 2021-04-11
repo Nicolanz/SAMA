4199072c779e06518f8e33ba3ed2b06625ac97b1
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-04-10 20:48:10 -0500

    fixing nav bugs

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,10 +1,17 @@
 var prevScrollpos = window.pageYOffset;
+var firstScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-145px";
+    document.getElementById("navbar").style.top = "-170px";
+  }
+  if (currentScrollPos > firstScrollpos  + 150 ){
+    document.getElementById("navbar").style.backgroundColor = "#D6EEF4";
+  } else if (currentScrollPos == firstScrollpos){
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+    document.getElementById("navbar").style.transition = "0.5s ease-in";
   }
   prevScrollpos = currentScrollPos;
 }
