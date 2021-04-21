92e237129fce07fc9d4100f3e7132b317cffcd4c
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-04-20 20:49:54 -0500

    Menu page

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,16 +2,18 @@ var prevScrollpos = window.pageYOffset;
 var firstScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
+var navElements = document.getElementsByClassName("navbar")
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+      navElements[0].style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-170px";
+      navElements[0].style.top = "-170px";
   }
   if (currentScrollPos > firstScrollpos  + 150 ){
-    document.getElementById("navbar").style.backgroundColor = "#D6EEF4";
+      navElements[0].style.backgroundColor = "#D6EEF4";
   } else if (currentScrollPos == firstScrollpos){
-    document.getElementById("navbar").style.backgroundColor = "transparent";
-    document.getElementById("navbar").style.transition = "0.5s ease-in";
+      navElements[0].style.backgroundColor = "transparent";
+      navElements[0].style.transition = "0.5s ease-in";
   }
+  console.log(navElements);
   prevScrollpos = currentScrollPos;
 }
