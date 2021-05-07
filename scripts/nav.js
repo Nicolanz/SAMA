84b6c9bb1a0b07296db54cda330b6c18434446f6
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-05-07 08:13:57 -0500

    Moving to jQuery

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,19 +1,21 @@
-var prevScrollpos = window.pageYOffset;
-var firstScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-var navElements = document.getElementsByClassName("navbar")
-  if (prevScrollpos > currentScrollPos) {
-      navElements[0].style.top = "0";
-  } else {
-      navElements[0].style.top = "-170px";
-  }
-  if (currentScrollPos > firstScrollpos  + 150 ){
-      navElements[0].style.backgroundColor = "#D6EEF4";
-  } else if (currentScrollPos == firstScrollpos){
-      navElements[0].style.backgroundColor = "transparent";
-      navElements[0].style.transition = "0.5s ease-in";
-  }
-  console.log(navElements);
-  prevScrollpos = currentScrollPos;
-}
+
+var prevScrollpos = $(window).scrollTop();
+var firstScrollpos = $(window).scrollTop();
+
+$(window).scroll(function(){
+
+    var currentScrollPos = $(window).scrollTop();
+
+    if (prevScrollpos > currentScrollPos) {
+        $("#navbar").css("top:", "0");
+    } else {
+        $("#navbar").css("top:", "-170");
+    }
+    if (currentScrollPos > firstScrollpos  + 150 ){
+        $("#navbar").css("background", "#D6EEF4");
+    } else if (currentScrollPos == firstScrollpos){
+        $("#navbar").css("background", "transparent");
+        $("#navbar").css("transition", "0.5s ease-in");
+    }
+    prevScrollpos = currentScrollPos;
+});
