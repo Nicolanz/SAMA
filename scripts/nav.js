e9395d4735b5873af962557745427e8a0a0a3fc3
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-05-07 16:44:32 -0500

    Colors and Sama Style

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,21 +1,29 @@
 
+
 var prevScrollpos = $(window).scrollTop();
 var firstScrollpos = $(window).scrollTop();
 
 $(window).scroll(function(){
 
     var currentScrollPos = $(window).scrollTop();
-
-    if (prevScrollpos > currentScrollPos) {
-        $("#navbar").css("top:", "0");
-    } else {
-        $("#navbar").css("top:", "-170");
+    if (currentScrollPos < prevScrollpos){
+        $("#navbar").css("top", "-125px");
+    }
+    else{
+        $("#navbar").css("top", "0");
     }
     if (currentScrollPos > firstScrollpos  + 150 ){
-        $("#navbar").css("background", "#D6EEF4");
-    } else if (currentScrollPos == firstScrollpos){
+        $("#navbar").css("min-height", "125px");
+        $("#navbar").css("background", "#eff4f8ef");
+        $("#navbar").css("box-shadow", "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px");
+        $("#navbar").css("transition", "0.3s ease-in");
+    }
+    else if (currentScrollPos <= firstScrollpos + 20){
+        $("#navbar").css("top", "0");
+        $("#navbar").css("min-height", "150px");
         $("#navbar").css("background", "transparent");
-        $("#navbar").css("transition", "0.5s ease-in");
+        $("#navbar").css("box-shadow", "initial");
+        $("#navbar").css("transition", "0.3s ease-in");
     }
     prevScrollpos = currentScrollPos;
 });
